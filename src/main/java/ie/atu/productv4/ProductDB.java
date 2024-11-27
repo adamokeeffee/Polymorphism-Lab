acb5aef0c9203f5749282f0fceda957737588514
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2024-11-27 17:35:05 +0000

    TV class complete

--- src/main/java/ie/atu/productv4/ProductDB.java
+++ src/main/java/ie/atu/productv4/ProductDB.java
@@ -1,12 +1,8 @@
 package ie.atu.productv4;
 
-import ie.atu.productv3.Book;
-import ie.atu.productv3.Product;
-import ie.atu.productv3.Software;
-
 public class ProductDB {
 
-    public static ie.atu.productv3.Product getProduct(String productCode) {
+    public static Product getProduct(String productCode) {
 
 
         Product myItem = null;
@@ -32,27 +28,44 @@ public class ProductDB {
             mySoftware.setVersion("Oracle 3.0");
             myItem = mySoftware;
         } else if (productCode.equalsIgnoreCase("java")) {
-            ie.atu.productv3.Book myBook = new ie.atu.productv3.Book();
+            Book myBook = new Book();
             myBook.setCode(productCode);
             myBook.setDescription("ATU Java Programming");
             myBook.setPrice(57.50);
             myBook.setAuthor("Joe Brown");
             myItem = myBook;
         } else if (productCode.equalsIgnoreCase("jsp")) {
-            ie.atu.productv3.Book myBook = new ie.atu.productv3.Book();
+            Book myBook = new Book();
             myBook.setCode(productCode);
             myBook.setDescription("Java Servlets and JSP");
             myBook.setPrice(57.50);
             myBook.setAuthor("Mike White");
             myItem = myBook;
         } else if (productCode.equalsIgnoreCase("mysql")) {
-            ie.atu.productv3.Book myBook = new Book();
+            Book myBook = new Book();
             myBook.setCode(productCode);
             myBook.setDescription("Lennon's MySQL");
             myBook.setPrice(54.50);
             myBook.setAuthor("Jim Lennon");
             myItem = myBook;
+        } else if (productCode.equalsIgnoreCase("Rap")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("FOEJOFJO");
+            myMusic.setPrice(40.99);
+            myMusic.setArtist("john");
+            myMusic.setLabel("joe");
+            myItem = myMusic;
+        }else if (productCode.equalsIgnoreCase("Country")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("fbjfhh");
+            myMusic.setPrice(101.56);
+            myMusic.setArtist("Christopher");
+            myMusic.setLabel("label land");
+            myItem = myMusic;
         }
+
         return myItem;
     }
 }
